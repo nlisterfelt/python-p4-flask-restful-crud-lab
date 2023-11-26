33d6618835fab5c5846eca4b079af90abf975b14
Nicole Listerfelt <nlisterfelt@gmail.com>
2023-11-26 17:06:27 -0600

    add delete and patch

--- server/app.py
+++ server/app.py
@@ -47,6 +47,19 @@ class PlantByID(Resource):
         plant = Plant.query.filter_by(id=id).first().to_dict()
         return make_response(jsonify(plant), 200)
 
+    def patch(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        form_data = request.get_json()
+        for attr in form_data:
+            setattr(plant, attr, form_data[attr])
+        db.session.commit()
+        return make_response(plant.to_dict(), 200)
+
+    def delete(self, id):
+        plant = Plant.query.filter_by(id=id).first()
+        db.session.delete(plant)
+        db.session.commit()
+        return make_response({'message':''}, 204)
 
 api.add_resource(PlantByID, '/plants/<int:id>')
 
